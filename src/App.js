9dc91daa330feea19c9589a9ade8593a476edd4e
eric23 <kh23ulmas@gmail.com>
2019-07-27 23:11:49 +0500

    Chang gitignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      todos: [
+        'Learn React',
+        'Do Laundry'
+      ],
+      todoToAdd: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleChange(event) {
+    const nextTodoToAdd = event.target.value;
+
+    this.setState({
+      todoToAdd: nextTodoToAdd
+    });
+  }
+  handleClick(event) {
+    const nextTodos = this.state.todos.concat(this.state.todoToAdd);
+    const nextTodoToAdd = '';
+
+    this.setState({
+      todos: nextTodos,
+      todoToAdd: nextTodoToAdd
+    });
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        </ul>
+        <input type="text" placeholder="New TODO" 
+        value={this.state.todoToAdd}
+        onChange={this.handleChange}/>
+        <button onClick={this.handleClick}>Add TODO</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
